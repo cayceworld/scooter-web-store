ef6674592aac25ea9bf61a327bf101ab99abe0b5
Sydorenko Vladyslav <cayceworld@outlook.com>
2022-07-30 10:51:31 +0100

    Add functional amount of items for accessory

--- src/components/features/Accessories/Accessory.js
+++ src/components/features/Accessories/Accessory.js
@@ -1,29 +1,49 @@
 import styles from './AccessoriesList.module.scss';
 import clsx from 'clsx';
-import { addToCart } from '../../../redux/cartRedux';
 import { useDispatch } from 'react-redux';
+import { addToCart, getCartProducts, addAmount } from '../../../redux/cartRedux';
+import { useSelector } from 'react-redux';
 
 const Accessory = props => {
 
-
+  const cart = useSelector(getCartProducts);
   const accessory = props;
 
 
   const dispatch = useDispatch();
+
   const addProduct = () => {
-    dispatch(addToCart({
-      title: props.title, id:props.id, 
-      image: props.image, price: props.price, category: 'accessory'
-    }))
+
+    if (cart.length == 0) {
+      dispatch(addToCart({
+        title: accessory.title, id: accessory.id,
+        image: accessory.image, price: accessory.price, category: 'accessory', amount: 1
+      }))
+    } else {
+      const filterCart = cart.find(item => item.id == accessory.id);
+
+      if (filterCart == undefined) {
+        dispatch(addToCart({
+          title: accessory.title, id: accessory.id,
+          image: accessory.image, price: accessory.price, category: 'accessory', amount: 1
+        }))
+        console.log('onemore')
+      } else if (filterCart.amount <= 9) {
+        dispatch(addAmount({ id: accessory.id, amount: 1 }))
+        console.log('onemore')
+      }
+    }
   }
+
+
 
 
   return (
     <div className={styles.Accessory} >
-      <div className={styles.Accessory__image}><img src={`${process.env.PUBLIC_URL}/images/Accessories/${props.image}`} /></div>
-      <div className={styles.Accessory__title}>{props.title}</div>
-      <div className={styles.Accessory__description}>{props.description}</div>
-      <div className={styles.Accessory__price}>${props.price}</div>
+      <div className={styles.Accessory__image}><img src={`${process.env.PUBLIC_URL}/images/Accessories/${accessory.image}`} /></div>
+      <div className={styles.Accessory__title}>{accessory.title}</div>
+      <div className={styles.Accessory__description}>{accessory.description}</div>
+      <div className={styles.Accessory__price}>${accessory.price}</div>
       <div className={styles.Accessory__button}>
 
         <button onClick={addProduct} className={clsx(styles.btn, accessory.inStock <= 0 && styles.btn_disabled)}>
--- src/components/features/SelectedProduct/SelectedProduct.js
+++ src/components/features/SelectedProduct/SelectedProduct.js
@@ -52,7 +52,7 @@ const SelectedProduct = () => {
     }
   }
 
-  //console.log(cart)
+
 
 
 
