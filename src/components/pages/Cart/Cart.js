2989f633a29530b93ff56aebeb964ab4ad8970bd
Sydorenko Vladyslav <cayceworld@outlook.com>
2022-08-03 20:01:29 +0100

    Add form validation

--- src/components/pages/Cart/Cart.js
+++ src/components/pages/Cart/Cart.js
@@ -7,9 +7,9 @@ import { useState } from 'react';
 import { updateAmountKickscooterRequest } from '../../../redux/kickscooterRedux';
 import { updateAmountAccessoryRequest } from '../../../redux/accessoriesRedux';
 import shortid from 'shortid';
+import { useForm } from "react-hook-form";
 
 const Cart = () => {
-
 
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
@@ -19,9 +19,10 @@ const Cart = () => {
   const cart = useSelector(getCartProducts);
   const dispatch = useDispatch();
 
+  const { register, handleSubmit: validate, formState: { errors } } = useForm();
 
-  const submit = e => {
-    e.preventDefault();
+
+  const submit = () => {
     dispatch(addOrder({ items: cart, userInfo: [name, phone, address], id: shortid.generate() }));
     dispatch(clearCart());
     cart.map(item => {
@@ -41,31 +42,37 @@ const Cart = () => {
   return (
     <div className={styles.cart}>
       <div className={styles.container}>
-
         <div>
           <h3 className={styles.cart__title}>Products in your Cart:</h3>
           <CartProducts />
-          {cart.length > 0 && <form className={styles.cart__form}>
+          {cart.length > 0 && <form className={styles.cart__form} onSubmit={validate(submit)}>
             <h3 className={styles.cart__title}>Delivery information:</h3>
-            <label>
-              <div>Name:</div>
-              <input type="text" name="name" className={styles.cart__input} value={name} onChange={e => setName(e.target.value)} />
-            </label>
-            <label>
-              <div>Phone:</div>
-              <input type="tel" name="tel" className={styles.cart__input} value={phone} onChange={e => setPhone(e.target.value)} />
-            </label>
-            <label>
-              <div>Address:</div>
-              <input type="text" name="address" className={styles.cart__input} value={address} onChange={e => setAddress(e.target.value)} />
-            </label>
-            <button className={styles.btn} onClick={submit}> submit order</button>
+            <div>Name:</div>
+            <input
+              {...register("name", { required: true })}
+              className={styles.cart__input}
+              value={name}
+              onChange={e => setName(e.target.value)} />
+            {errors.name?.type === 'required' && <span>Name is required</span>}
+
+            <div>Phone:</div>
+            <input {...register("tel", { required: true })}
+              type="tel"
+              className={styles.cart__input} value={phone}
+              onChange={e => setPhone(e.target.value)} />
+            {errors.tel && <span>Phone is required</span>}
+
+            <div>Address:</div>
+            <input {...register("address", { required: " Address is required" })}
+              type="text"
+              className={styles.cart__input} value={address}
+              onChange={e => setAddress(e.target.value)} />
+            <span>{errors.address?.message}</span>
+            <button className={styles.btn} type='submit'> submit order</button>
           </form>}
         </div>
-
       </div>
-
     </div>);
 }
 
-export default Cart;
+export default Cart
